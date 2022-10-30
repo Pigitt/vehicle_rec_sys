afedb2e863cce85bf993a9c3e7e2ef63697ae3ac
pigitt <hbdjkehd@gmail.com>
2022-10-30 17:16:19 -0500

    change

--- vehicles_rec_server/conf/proj_path.py
+++ vehicles_rec_server/conf/proj_path.py
@@ -5,7 +5,7 @@ proj_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath('__f
 stop_words_path = proj_path + "conf/stop_words.txt"
 bad_case_vehicle_log_path = proj_path + "logs/vehicle_bad_cases.log"
 
-root_data_path = "D:/git/Pigitt/vehicle_rec_sys/vehicles_data"
-log_data_path = root_data_path + "train_data_5w.csv"
+root_data_path = "D:/git/Pigitt/vehicle_rec_sys/off_data"
+log_data_path = root_data_path + "train_data_30w.txt"
 doc_info_path = root_data_path + "doc_info.txt"
-user_info_path = root_data_path + "user_info_data_5w.csv" 
+user_info_path = root_data_path + "user_info.txt" 
